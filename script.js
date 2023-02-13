d231e00721417a0ce37ce332ccb2364541893df7
MomirE3 <79815849+MomirE3@users.noreply.github.com>
2023-02-13 02:00:57 +0100

    added basic informations on front

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-button = document.querySelector("#searchButton");
-button.addEventListener("click", () => {
-  text = document.querySelector("#inputValue").value;
-  console.log(text);
+let button = document.querySelector("#searchButton");
 
+let text = document.querySelector("#inputValue");
+text.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    button.click();
+  }
+});
+
+button.addEventListener("click", () => {
+  let text = document.querySelector("#inputValue").value;
   fetch(
-    `http://api.weatherapi.com/v1/current.json?key=5afd5d58c8504bb4bc2224735231102&q=${text}&aqi=yes`
+    `http://api.weatherapi.com/v1/forecast.json?key=5afd5d58c8504bb4bc2224735231102&q=${text}&days=1&aqi=no&alerts=no`
   )
     .then((response) => response.json())
     .then((data) => {
-      locationAPI = data["location"];
-      tempAPI = data["current"];
-      nameOfCity = document.querySelector("#name");
-      nameOfCity.innerHTML = locationAPI.name;
-      tempOfCity = document.querySelector("#temp");
-      tempOfCity.innerHTML = tempAPI.temp_c;
+      console.log(data);
+      let locationAPI = data["location"];
+      let currentAPI = data["current"];
+
+      let nameOfCity = document.querySelector("#name");
+      nameOfCity.append(locationAPI.name);
+
+      let date = locationAPI.localtime.slice(0, 10);
+
+      let dateOfCity = document.querySelector("#date");
+      dateOfCity.append(newFormatDate(date));
     });
   document.querySelector("#inputValue").value = "";
 });
+
+function newFormatDate(date) {
+  let newDate = new Date(date);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return newDate.toLocaleString("en-US", options);
+}
